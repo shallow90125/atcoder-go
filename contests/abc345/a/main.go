a969bbe8d64f673442453837de6b81e75edcfaf2
shallow90125 <shallow90125@gmail.com>
2024-03-27 14:14:05 +0900

    abc345 a

---
+++ contests/abc345/a/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var r = bufio.NewScanner(os.Stdin)
+var w = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer flush()
+
+	s := is()
+
+	if s[0] != byte('<') {
+		out("No")
+		return
+	}
+
+	if s[len(s)-1] != byte('>') {
+		out("No")
+		return
+	}
+
+	for i := 1; i < len(s)-1; i++ {
+		if s[i] != byte('=') {
+			out("No")
+			return
+		}
+	}
+
+	out("Yes")
+}
+
+func init() {
+	r.Buffer([]byte{}, math.MaxInt64)
+	r.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := os.ReadFile("./input.txt")
+		if e != nil {
+			panic(e)
+		}
+		r = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
+	}
+}
+
+func flush() {
+	e := w.Flush()
+	if e != nil {
+		panic(e)
+	}
+}
+
+func in() int {
+	r.Scan()
+	i, e := strconv.Atoi(r.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func out(v ...interface{}) {
+	_, e := fmt.Fprintln(w, v...)
+	if e != nil {
+		panic(e)
+	}
+}
+
+func is() []byte {
+	r.Scan()
+	return r.Bytes()
+}
