2455e850c92db68c4cf3e5c48d314d3d90dc4c95
shallow90125 <shallow90125@gmail.com>
2024-04-28 19:12:05 +0900

    abc351 b

---
+++ contests/abc351/b/main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var r = bufio.NewScanner(os.Stdin)
+var w = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer flush()
+
+	n := in()
+
+	m := make(map[int]map[int]byte)
+
+	for y := 1; y <= n; y++ {
+		m[y] = make(map[int]byte)
+		a := ib()
+		for x := 1; x <= n; x++ {
+			m[y][x] = a[x-1]
+		}
+	}
+
+	for y := 1; y <= n; y++ {
+		b := ib()
+		for x := 1; x <= n; x++ {
+			if m[y][x] != b[x-1] {
+				out(y, x)
+				return
+			}
+		}
+	}
+}
+
+const inf = math.MaxInt64
+
+func init() {
+	r.Buffer([]byte{}, math.MaxInt64)
+	r.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := os.ReadFile("./input.txt")
+		if e != nil {
+			panic(e)
+		}
+		r = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
+	}
+}
+
+func flush() {
+	e := w.Flush()
+	if e != nil {
+		panic(e)
+	}
+}
+
+func out(v ...interface{}) {
+	_, e := fmt.Fprintln(w, v...)
+	if e != nil {
+		panic(e)
+	}
+}
+
+func in() int {
+	r.Scan()
+	i, e := strconv.Atoi(r.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func in2() (int, int) {
+	return in(), in()
+}
+
+func in3() (int, int, int) {
+	return in(), in(), in()
+}
+
+func inn(n int) []int {
+	v := make([]int, n)
+	for i := 0; i < n; i++ {
+		v[i] = in()
+	}
+	return v
+}
+
+func ib() []byte {
+	r.Scan()
+	return r.Bytes()
+}
+
+func ibn(n int) [][]byte {
+	v := make([][]byte, n)
+	for i := 0; i < n; i++ {
+		v[i] = ib()
+	}
+	return v
+}
+
+func is() string {
+	r.Scan()
+	return r.Text()
+}
+
+func isn(n int) []string {
+	v := make([]string, n)
+	for i := 0; i < n; i++ {
+		v[i] = is()
+	}
+	return v
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func abs(a int) int {
+	if 0 < a {
+		return a
+	}
+	return -a
+}
